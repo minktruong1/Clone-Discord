df06af9d7d690a01f13232b745240ccaacde40de
minktruong1 <minhpro207@yahoo.com>
2023-12-19 01:46:18 +0700

    Khoi tao du an

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+  return <div>LoginRegisterPage</div>;
+};
+
+export default RegisterPage;
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-blue-500 h-screen w-full">{children}</div>;
+};
+
+export default AuthLayout;
